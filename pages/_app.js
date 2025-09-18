c264b9a11a11ef2d0d6f9c475d711d05e54d6b52
aquraiq-site <aquraiq@gmail.com>
2025-09-18 15:32:52 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import Layout from "../components/Layout";
-import "../styles/globals.css"; // اگر استایل داری
+import "../styles/globals.css"; // بعداً درست می‌کنیم
 
 export default function MyApp({ Component, pageProps }) {
   return (
