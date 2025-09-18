515d75825195f33fb1f955bbd70d37b64ee79378
aquraiq-site <aquraiq@gmail.com>
2025-09-18 15:32:22 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,10 +4,10 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Navbar />
-      <main>{children}</main>
+      <main style={{ flex: "1" }}>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
