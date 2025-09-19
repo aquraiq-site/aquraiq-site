47d7b250d141ce74c210a6c76887d1957d980d8b
aquraiq-site <aquraiq@gmail.com>
2025-09-19 11:11:29 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
-// pages/_app.js
+import '../styles/globals.css'
 
-import Layout from "../components/Layout";
-import "../styles/globals.css"; // گلوبال استایل
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
