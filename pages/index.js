9e15599af12e4ecf16157a285efa7984ab7fcbb4
aquraiq-site <aquraiq@gmail.com>
2025-09-19 09:33:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
-import Navbar from "../components/Navbar";
+// pages/index.js
+
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Ranking from "../components/Ranking";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <>
       <Hero />
       <Features />
       <Ranking />
-      <Footer />
-    </div>
+    </>
   );
 }
